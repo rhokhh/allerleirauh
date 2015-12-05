fdf2db50face58f2e4ef4a4665c1105440006213
Fabian Gebert <fabian@gebert.hamburg>
2015-12-05 12:53:03 +0100

    init version of module

---
+++ src/classes/book.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   BookLibrary
+ * @author    RHOK
+ * @license   Apache2.0
+ * @copyright RHOK
+ */
+
+
+/**
+ * Namespace
+ */
+namespace BookLibrary;
+
+
+/**
+ * Class book
+ *
+ * @copyright  RHOK
+ * @author     RHOK
+ * @package    Devtools
+ */
+class book extends \Module
+{
+
+	/**
+	 * Template
+	 * @var string
+	 */
+	protected $strTemplate = '';
+
+
+	/**
+	 * Generate the module
+	 */
+	protected function compile()
+	{
+
+	}
+}
---
+++ src/languages/en/default.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   BookLibrary
+ * @author    RHOK
+ * @license   Apache2.0
+ * @copyright RHOK
+ */
+
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['MSC'][''] = '';
