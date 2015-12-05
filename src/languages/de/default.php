ae95836c73f9090afe237b31dd78eeb03b65a289
Nils Schmidt <nils.schmidt.ger@gmail.com>
2015-12-05 18:14:11 +0100

    DropDown for Tags in Backend

--- src/languages/de/default.php
+++ src/languages/de/default.php
@@ -29,3 +29,53 @@ $GLOBALS['TL_LANG']['bl_book']['tag'] = array("Schlagwort","");
 
 $GLOBALS['TL_LANG']['CTE']['book_library'] = array("Bibliothek", "Bibliothek");
 $GLOBALS['TL_LANG']['CTE']['book_gallery'] = array("Buchliste","Liste von Büchern");
+
+
+$GLOBALS['TL_LANG']['book_library']['tags']['K'] = "Krimis";
+$GLOBALS['TL_LANG']['book_library']['tags']['KB'] = "KinderBücher";
+$GLOBALS['TL_LANG']['book_library']['tags']['RE'] = "Romane für Erwachsene";
+$GLOBALS['TL_LANG']['book_library']['tags']['RJ'] = "Romane für Jugendliche";
+
+$GLOBALS['TL_LANG']['book_library']['tags']['B'] = "Beratungsführer";
+$GLOBALS['TL_LANG']['book_library']['tags']['BM'] = "Borderline / MPS";
+$GLOBALS['TL_LANG']['book_library']['tags']['Es'] = "Essstörungen";
+$GLOBALS['TL_LANG']['book_library']['tags']['FG'] = "Frauen und Gewalt";
+$GLOBALS['TL_LANG']['book_library']['tags']['Ki'] = "Kindesmisshandlung";
+$GLOBALS['TL_LANG']['book_library']['tags']['MA'] = "Mädchen / MädchenArbeit";
+$GLOBALS['TL_LANG']['book_library']['tags']['MBG'] = "Methoden für Beratung und Gruppe";
+$GLOBALS['TL_LANG']['book_library']['tags']['MM'] = "Methoden in der Mädchenarbeit";
+$GLOBALS['TL_LANG']['book_library']['tags']['MW'] = "Mädchen und Wohnen";
+$GLOBALS['TL_LANG']['book_library']['tags']['PNM'] = "Prävention Neue Medien";
+$GLOBALS['TL_LANG']['book_library']['tags']['Pa'] = "Prävention allgemein";
+$GLOBALS['TL_LANG']['book_library']['tags']['PAuf'] = "Prävention Aufklärungsbücher";
+$GLOBALS['TL_LANG']['book_library']['tags']['PG'] = "Prävention Gender";
+$GLOBALS['TL_LANG']['book_library']['tags']['PH'] = "Prävention Handicap";
+$GLOBALS['TL_LANG']['book_library']['tags']['PK'] = "Prävention Kinder";
+$GLOBALS['TL_LANG']['book_library']['tags']['PrS'] = "Prostitution / Sucht";
+$GLOBALS['TL_LANG']['book_library']['tags']['Psa'] = "Psychologie allgemein";
+$GLOBALS['TL_LANG']['book_library']['tags']['PSch'] = "Prävention Schule";
+$GLOBALS['TL_LANG']['book_library']['tags']['PSP'] = "Prävention SexualPädagogik";
+$GLOBALS['TL_LANG']['book_library']['tags']['rMS'] = "ritueller Missbrauch / Sekten";
+$GLOBALS['TL_LANG']['book_library']['tags']['sMa'] = "sexueller Missbrauch allgemein";
+$GLOBALS['TL_LANG']['book_library']['tags']['sMI'] = "sexueller Missbrauch und Institutionen";
+$GLOBALS['TL_LANG']['book_library']['tags']['sMJ'] = "sexueller Missbrauch und Justiz";
+$GLOBALS['TL_LANG']['book_library']['tags']['sMJu'] = "sexueller Missbrauch von Jungen";
+$GLOBALS['TL_LANG']['book_library']['tags']['sMM'] = "sexueller Missbrauch und Mütter";
+$GLOBALS['TL_LANG']['book_library']['tags']['sMNa'] = "sexueller Missbrauch und Nachschlagewerke";
+$GLOBALS['TL_LANG']['book_library']['tags']['sMÖ'] = "sexueller Missbrauch und Öffentlichkeit";
+$GLOBALS['TL_LANG']['book_library']['tags']['sMSe'] = "sexueller Missbrauch und Selbsthilfe";
+$GLOBALS['TL_LANG']['book_library']['tags']['sMT'] = "sexueller Missbrauch und Therapie";
+$GLOBALS['TL_LANG']['book_library']['tags']['sMTä'] = "sexueller Missbrauch und TäterInnen";
+$GLOBALS['TL_LANG']['book_library']['tags']['sMTKJ'] = "sexueller Missbrauch / Therapie von Kindern und Jugendlichen";
+$GLOBALS['TL_LANG']['book_library']['tags']['sV'] = "selbstverletzendes Verhalten";
+$GLOBALS['TL_LANG']['book_library']['tags']['Ta'] = "Therapie allgemein";
+$GLOBALS['TL_LANG']['book_library']['tags']['TKJ'] = "Therapie von Kindern und Jugendlichen";
+$GLOBALS['TL_LANG']['book_library']['tags']['TP '] = "TraumaPädagogik";
+$GLOBALS['TL_LANG']['book_library']['tags']['Tr'] = "Trauma";
+$GLOBALS['TL_LANG']['book_library']['tags']['V'] = "Vergewaltigung";
+
+
+$GLOBALS['TL_LANG']['book_library']['tags']['Ö'] = "Öffentlichkeitsarbeit";
+$GLOBALS['TL_LANG']['book_library']['tags']['G'] = "Gesetze";
+$GLOBALS['TL_LANG']['book_library']['tags']['L'] = "Lexika";
+$GLOBALS['TL_LANG']['book_library']['tags']['AE'] = "Arbeitsorganisation und Evaluation";
